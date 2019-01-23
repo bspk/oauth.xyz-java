a9d00d3ee1fba451745f50f2fa866c783e1549e5
Justin Richer <justin@bspk.io>
2019-01-23 17:00:34 -0500

    default json in/out

--- src/main/js/client.js
+++ src/main/js/client.js
@@ -11,9 +11,10 @@ const registry = baseRegistry.child();
 
 registry.register('text/uri-list', require('./api/uriListConverter'));
 registry.register('application/hal+json', require('rest/mime/type/application/hal'));
+registry.register('application/json', require('rest/mime/type/application/json'));
 
 module.exports = rest
 		.wrap(mime, { registry: registry })
 		.wrap(uriTemplateInterceptor)
 		.wrap(errorCode)
-		.wrap(defaultRequest, { headers: { 'Accept': 'application/hal+json' }});
+		.wrap(defaultRequest, { headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }});
