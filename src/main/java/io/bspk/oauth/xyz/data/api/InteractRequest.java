55cbeb5d9cfee959b3f8bf796010677f51dc22f6
Justin Richer <justin@bspk.io>
2019-01-23 17:00:58 -0500

    wrap API objects with data accessors

--- src/main/java/io/bspk/oauth/xyz/data/api/InteractRequest.java
+++ src/main/java/io/bspk/oauth/xyz/data/api/InteractRequest.java
@@ -1,13 +1,19 @@
 package io.bspk.oauth.xyz.data.api;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import io.bspk.oauth.xyz.data.Interact.Type;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 /**
  * @author jricher
  *
  */
 @Data
+@Accessors(chain = true)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class InteractRequest {
 
 	private Type type;
