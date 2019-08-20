21a706da6a9f8cea3a60e988e68c629752a53659
Justin Richer <justin@bspk.io>
2019-08-20 16:51:32 -0400

    mark inbound and outbound HTTP in logs

--- src/main/java/io/bspk/oauth/xyz/Application.java
+++ src/main/java/io/bspk/oauth/xyz/Application.java
@@ -103,21 +103,21 @@ public class Application {
 	    }
 
 	    private void logRequest(HttpRequest request, byte[] body) throws IOException {
-	            log.info("===========================request begin================================================");
+	            log.info("<<<========================request begin================================================");
 	            log.info("URI         : {}", request.getURI());
 	            log.info("Method      : {}", request.getMethod());
 	            log.info("Headers     : {}", request.getHeaders());
 	            log.info("Request body: {}", new String(body, "UTF-8"));
-	            log.info("==========================request end================================================");
+	            log.info("<<<=======================request end================================================");
 	    }
 
 	    private void logResponse(ClientHttpResponse response) throws IOException {
-	            log.info("============================response begin==========================================");
+	            log.info(">>>=========================response begin==========================================");
 	            log.info("Status code  : {}", response.getStatusCode());
 	            log.info("Status text  : {}", response.getStatusText());
 	            log.info("Headers      : {}", response.getHeaders());
 	            log.info("Response body: {}", StreamUtils.copyToString(response.getBody(), Charset.defaultCharset()));
-	            log.info("=======================response end=================================================");
+	            log.info(">>>====================response end=================================================");
 	    }
 	}
 
